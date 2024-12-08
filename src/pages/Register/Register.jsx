2a27904f622a7b67b226f32c2954270d03265b08
Programming Hero <programming.hero1@gmail.com>
2024-12-07 21:18:27 -0600

    firebase config added

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,30 +1,39 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import registerLottieData from '../../assets/lottie/register.json';
 
 const Register = () => {
+
+    const handleRegister = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        console.log(email, password);
+
+        // password validation: 
+    }
+
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
+                <div className="text-center lg:text-left w-96">
+                    <Lottie animationData={registerLottieData}></Lottie>
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                <h1 className="ml-8 mt-4 text-5xl font-bold">Register now!</h1>
-                    <form className="card-body">
+                    <h1 className="ml-8 mt-4 text-5xl font-bold">Register now!</h1>
+                    <form onSubmit={handleRegister} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" required />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" required />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
