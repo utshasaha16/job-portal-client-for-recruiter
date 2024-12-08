e6044928efdb90c45b821969800dbfa9b582b58d
Programming Hero <programming.hero1@gmail.com>
2024-12-07 21:01:21 -0600

    added navbar register route

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,25 @@
 import {
     createBrowserRouter,
   } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+import Home from "../pages/Home/Home";
+import Register from "../pages/Register/Register";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      errorElement: <h2>Route not found</h2>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+        {
+            path: 'register',
+            element: <Register></Register>
+        }
+      ]
     },
   ]);
 
