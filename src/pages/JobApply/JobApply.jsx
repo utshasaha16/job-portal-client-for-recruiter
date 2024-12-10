57f763a6e55f1bdd3d29d07621d66952dfb2513e
Programming Hero <programming.hero1@gmail.com>
2024-12-10 17:10:04 -0600

    save job application to the server

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
     const { id } = useParams();
@@ -23,7 +24,27 @@ const JobApply = () => {
             github,
             resume
         }
-        
+
+        fetch('http://localhost:5000/job-applications', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(jobApplication)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Your work has been saved",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+
     }
 
     return (
