b6f42d991a8d1cfcff4ac05d4a9c3406148ae914
Programming Hero <programming.hero1@gmail.com>
2024-12-10 05:40:00 -0600

    get job detail data

--- src/pages/Home/HotJobCard.jsx
+++ src/pages/Home/HotJobCard.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FaDollarSign, FaMapMarkerAlt } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const HotJobCard = ({ job }) => {
-    const { title, company, company_logo, requirements, description, location, salaryRange } = job;
+    const { _id, title, company, company_logo, requirements, description, location, salaryRange } = job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
             <div className='flex gap-2 m-2'>
@@ -24,16 +25,19 @@ const HotJobCard = ({ job }) => {
                 <p>{description}</p>
                 <div className='flex gap-2 flex-wrap'>
                     {
-                        requirements.map(skill => <p
+                        requirements.map((skill, index) => <p
+                            key={index}
                             className='border rounded-md text-center px-2 hover:text-purple-600 hover:bg-gray-400'
                         >{skill}</p>)
                     }
                 </div>
                 <div className="card-actions justify-end items-center mt-4">
 
-                    <p className='flex items-center'>Salary: <FaDollarSign></FaDollarSign> {salaryRange.min} - {salaryRange.max} {salaryRange.currency }</p>
+                    <p className='flex items-center'>Salary: <FaDollarSign></FaDollarSign> {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
 
-                    <button className="btn btn-primary">Apply</button>
+                    <Link to={`/jobs/${_id}`}>
+                        <button className="btn btn-primary">Apply</button>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,31 +1,37 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter,
+} from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
 import Home from "../pages/Home/Home";
 import Register from "../pages/Register/Register";
 import SignIn from "../pages/SignIn/SignIn";
+import JobDetails from "../pages/JobDetails/JobDetails";
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout></MainLayout>,
-      errorElement: <h2>Route not found</h2>,
-      children: [
-        {
-            path: '/',
-            element: <Home></Home>
-        },
-        {
-            path: 'register',
-            element: <Register></Register>
-        },
-        {
-          path: 'signIn',
-          element: <SignIn></SignIn>
-        }
-      ]
-    },
-  ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <h2>Route not found</h2>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/jobs/:id',
+        element: <JobDetails></JobDetails>,
+        loader: ({ params }) => fetch(`http://localhost:5000/jobs/${params.id}`)
+      },
+      {
+        path: 'register',
+        element: <Register></Register>
+      },
+      {
+        path: 'signIn',
+        element: <SignIn></SignIn>
+      }
+    ]
+  },
+]);
 
-  export default  router;
+export default router;
