f709ad1223cce5c0c444bffbf8a739b55bffde3f
Programming Hero <programming.hero1@gmail.com>
2024-12-09 20:36:35 -0600

    added job info in the job card

--- src/pages/Home/HotJobCard.jsx
+++ src/pages/Home/HotJobCard.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { FaDollarSign, FaMapMarkerAlt } from 'react-icons/fa';
 
-const HotJobCard = () => {
+const HotJobCard = ({ job }) => {
+    const { title, company, company_logo, requirements, description, location, salaryRange } = job;
     return (
-        <div>
-            
+        <div className="card card-compact bg-base-100 shadow-xl">
+            <div className='flex gap-2 m-2'>
+                <figure>
+                    <img
+                        className='w-16'
+                        src={company_logo}
+                        alt="Shoes" />
+                </figure>
+                <div>
+                    <h4 className="text-2xl">{company}</h4>
+                    <p className='flex gap-1 items-center'> <FaMapMarkerAlt></FaMapMarkerAlt> {location}</p>
+                </div>
+            </div>
+            <div className="card-body">
+                <h2 className="card-title">{title}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>{description}</p>
+                <div className='flex gap-2 flex-wrap'>
+                    {
+                        requirements.map(skill => <p
+                            className='border rounded-md text-center px-2 hover:text-purple-600 hover:bg-gray-400'
+                        >{skill}</p>)
+                    }
+                </div>
+                <div className="card-actions justify-end items-center mt-4">
+
+                    <p className='flex items-center'>Salary: <FaDollarSign></FaDollarSign> {salaryRange.min} - {salaryRange.max} {salaryRange.currency }</p>
+
+                    <button className="btn btn-primary">Apply</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Home/HotJobs.jsx
+++ src/pages/Home/HotJobs.jsx
@@ -14,7 +14,7 @@ const HotJobs = () => {
 
     return (
         <div>
-            <div>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
                 {
                     jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
                 }
