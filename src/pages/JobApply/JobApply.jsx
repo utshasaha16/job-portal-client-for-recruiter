b6e70b8502572c880bbf673533e19df0837372c4
Programming Hero <programming.hero1@gmail.com>
2024-12-10 20:35:11 -0600

    added my application

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import Swal from 'sweetalert2';
 
 const JobApply = () => {
     const { id } = useParams();
     const { user } = useAuth();
+    const navigate = useNavigate();
     // console.log(id, user);
 
     const submitJobApplication = e => {
@@ -42,6 +43,7 @@ const JobApply = () => {
                         showConfirmButton: false,
                         timer: 1500
                     });
+                    navigate('/myApplications')
                 }
             })
 
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,6 +8,7 @@ import SignIn from "../pages/SignIn/SignIn";
 import JobDetails from "../pages/JobDetails/JobDetails";
 import PrivateRoute from "./PrivateRoute";
 import JobApply from "../pages/JobApply/JobApply";
+import MyApplications from "../pages/MyApplications/MyApplications";
 
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
       {
         path: '/jobApply/:id',
         element: <PrivateRoute><JobApply></JobApply></PrivateRoute>
+      },
+      {
+        path: '/myApplications',
+        element: <PrivateRoute><MyApplications></MyApplications></PrivateRoute>
       },
       {
         path: 'register',
