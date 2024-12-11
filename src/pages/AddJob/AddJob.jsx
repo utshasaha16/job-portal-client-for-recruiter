6ecabee24e83c7e99204c68df3b2f3b41f39571b
Programming Hero <programming.hero1@gmail.com>
2024-12-11 05:55:32 -0600

    added a few form field in the add job post form

--- src/pages/AddJob/AddJob.jsx
+++ src/pages/AddJob/AddJob.jsx
@@ -19,8 +19,82 @@ const AddJob = () => {
                     </label>
                     <input type="text" name='location' placeholder="Job Location" className="input input-bordered" required />
                 </div>
+                {/* job Type */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Type</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a Job type</option>
+                        <option>Full-time</option>
+                        <option>Intern</option>
+                        <option>Part-time</option>
+                    </select>
+                </div>
+                {/* job Type */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Field</span>
+                    </label>
+                    <select className="select select-ghost w-full max-w-xs">
+                        <option disabled selected>Pick a Job Field</option>
+                        <option>Engineering</option>
+                        <option>Marketing</option>
+                        <option>Finance</option>
+                        <option>Teaching</option>
+                    </select>
+                </div>
+                {/* salary range */}
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 items-end'>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Salary Range</span>
+                        </label>
+                        <input type="text" name='min' placeholder="Min" className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <input type="text" name='max' placeholder="Max " className="input input-bordered" required />
+                    </div>
+                    <div className="form-control">
+                        <select className="select select-ghost w-full max-w-xs">
+                            <option disabled selected>Currency</option>
+                            <option>BDT</option>
+                            <option>USD</option>
+                            <option>INR</option>
+                        </select>
+                    </div>
+                </div>
+                {/* Job Description */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Description</span>
+                    </label>
+                    <textarea className="textarea textarea-bordered" placeholder="Job Description" name="description" required></textarea>
+                </div>
+                {/* Company Name */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Company Name</span>
+                    </label>
+                    <input type="text" name='company' placeholder="Company Name" className="input input-bordered" required />
+                </div>
+                {/* requirements */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Requirements</span>
+                    </label>
+                    <textarea className="textarea textarea-bordered" placeholder="put each requirements in a new line" name="description" required></textarea>
+                </div>
+                {/* responsibilities */}
+                <div className="form-control">
+                    <label className="label">
+                        <span className="label-text">Job Responsibilities</span>
+                    </label>
+                    <textarea className="textarea textarea-bordered" placeholder="Write each responsibility in a new line" name="description" required></textarea>
+                </div>
+                {/* submit button */}
                 <div className="form-control mt-6">
-                    <button className="btn btn-primary">Login</button>
+                    <button className="btn btn-primary">Submit</button>
                 </div>
             </form>
         </div>
