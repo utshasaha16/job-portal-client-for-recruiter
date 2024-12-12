187b2095445290097a42dadf0862c34ac77e487f
Programming Hero <programming.hero1@gmail.com>
2024-12-11 22:49:18 -0600

    update job application status

--- src/pages/ViewApplications/ViewApplications.jsx
+++ src/pages/ViewApplications/ViewApplications.jsx
@@ -1,8 +1,36 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ViewApplications = () => {
     const applications = useLoaderData();
+
+    const handleStatusUpdate = (e, id) => {
+        console.log(e.target.value, id)
+        const data = {
+            status: e.target.value
+        }
+        fetch(`http://localhost:5000/job-applications/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.modifiedCount) {
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: "Status Has been updated.",
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
+    }
+
     return (
         <div>
             <h2 className="text-3xl">Applications for this job: {applications.length}</h2>
@@ -13,8 +41,8 @@ const ViewApplications = () => {
                         <tr>
                             <th></th>
                             <th>Email</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
+                            <th>Status</th>
+                            <th>Update Status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -23,7 +51,18 @@ const ViewApplications = () => {
                                 <th>{index + 1}</th>
                                 <td>{app.applicant_email}</td>
                                 <td>Quality Control Specialist</td>
-                                <td>Blue</td>
+                                <td>
+                                    <select
+                                        onChange={(e) => handleStatusUpdate(e, app._id)}
+                                        defaultValue={app.status || 'Change Status'}
+                                        className="select select-bordered select-xs w-full max-w-xs">
+                                        <option disabled>Change Status</option>
+                                        <option>Under Review</option>
+                                        <option>Set Interview</option>
+                                        <option>Hired</option>
+                                        <option>Rejected</option>
+                                    </select>
+                                </td>
                             </tr>)
                         }
                     </tbody>
