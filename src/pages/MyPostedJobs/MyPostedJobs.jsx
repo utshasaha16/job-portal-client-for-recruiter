a259a061aa1881f94c716d37b0320a5e2eec5f6e
Programming Hero <programming.hero1@gmail.com>
2024-12-11 21:34:25 -0600

    show my posted job lists

--- src/pages/MyPostedJobs/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs/MyPostedJobs.jsx
@@ -14,6 +14,30 @@ const MyPostedJobs = () => {
     return (
         <div>
             <h2 className='text-3xl'>My Posted Jobs: {jobs.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Job Title</th>
+                            <th>Deadline</th>
+                            <th>Application Count</th>
+                            <th>Favorite Color</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            jobs.map((job, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{job.title}</td>
+                                <td>{job.applicationDeadline}</td>
+                                <td>{job.applicationCount}</td>
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
