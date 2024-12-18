66d683d55c010f9f3349ad0e278531a1acde7a98
utshasaha16 <utsharoy12345@gmail.com>
2024-12-19 02:40:24 +0600

    unverified user navigate to sign in page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useEffect } from 'react';
+import useAuth from './useAuth';
+import { useNavigate } from 'react-router-dom';
 
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:5000',
@@ -7,6 +9,28 @@ const axiosInstance = axios.create({
 });
 
 const useAxiosSecure = () => {
+    const { signOutUser } = useAuth();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        axiosInstance.interceptors.response.use(response => {
+            return response
+        }, error => {
+            console.log('Error caught in interceptor', error);
+
+            if (error.status === 401 || error.status === 403) {
+                console.log('need to log out the user');
+                signOutUser()
+                .then(() => {
+                    console.log('logged out user');
+                    navigate('/signIn');
+                }).catch(err => console.log(err))
+            }
+
+            return Promise.reject(error)
+        })
+    }, [])
+
     return axiosInstance;
 };
 
